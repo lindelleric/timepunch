8d196d8067bd8c2836bd52959f3b4e643587d4b1
Eric Nilsson <eric.nilsson@hotmail.com>
2020-05-13 12:39:13 +0200

    Create server an route for logging arrival or depart to logfile

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import * as Hapi from "@hapi/hapi";
+
+import * as fs from "fs";
+
+const logFilePath = 'logfile.log';
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+
+    server.route({
+        method: 'GET',
+        path: '/log',
+        handler: (request, h) => {
+            fs.appendFileSync(logFilePath, `${new Date().toLocaleString()} ${request.query.type} \n`);
+            return 'OK';
+        }
+    });
+
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
